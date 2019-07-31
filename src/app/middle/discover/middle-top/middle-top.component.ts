6f299436f42702fd85fa4b88e05e4d6d5357acbf
qinzhihao-conghua <1043419697@qq.com>
2019-07-31 21:33:15 +0800

    滚动图片以及布局的修改

--- src/app/middle/discover/middle-top/middle-top.component.ts
+++ src/app/middle/discover/middle-top/middle-top.component.ts
@@ -10,6 +10,35 @@ export class MiddleTopComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.setScroll();
+  }
+
+  images: any[] = [
+    { scroll: "../../../../assets/images/1.jpg", bgc: "../../../../assets/images/1-1.jpg" },
+    { scroll: "../../../../assets/images/2.jpg", bgc: "../../../../assets/images/2-1.jpg" },
+    { scroll: "../../../../assets/images/3.jpg", bgc: "../../../../assets/images/3-1.jpg" },
+    { scroll: "../../../../assets/images/4.jpg", bgc: "../../../../assets/images/4-1.jpg" },
+    { scroll: "../../../../assets/images/5.jpg", bgc: "../../../../assets/images/5-1.jpg" },
+    { scroll: "../../../../assets/images/6.jpg", bgc: "../../../../assets/images/6-1.jpg" },
+    { scroll: "../../../../assets/images/7.jpg", bgc: "../../../../assets/images/7-1.jpg" },
+    { scroll: "../../../../assets/images/8.jpg", bgc: "../../../../assets/images/8-1.jpg" },
+    { scroll: "../../../../assets/images/9.jpg", bgc: "../../../../assets/images/9-1.jpg" }
+  ]
+  imageSrc: string = "../../../../assets/images/1.jpg";
+  imageBgc: string = "";
+  setScroll() {
+    let i = 1;
+    setInterval(() => {
+      if (i == this.images.length) {
+        i = 0;
+      }
+      this.imageSrc = this.images[i].scroll;
+      this.imageBgc = "url(" + this.images[i].bgc + ")";
+      i++;
+    }, 4000);
+  }
+  locationImage(i){
+    console.log(i);
   }
 
 }
