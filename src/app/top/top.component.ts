11f7ab559e2c46cf64405dc68f176af6d13f834e
qinzhihao-conghua <1043419697@qq.com>
2019-08-04 11:11:59 +0800

    布局错误的更改

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../service/http.service';
-import { Route, Router, ActivatedRoute } from '@angular/router';
+import { Route, Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { PlatformLocation } from '@angular/common';
 
 @Component({
   selector: 'app-top',
@@ -11,19 +12,34 @@ export class TopComponent implements OnInit {
 
   constructor(
     private httpservice: HttpService,
-    private route:ActivatedRoute
+    private route: Router,
+    private planform: PlatformLocation
   ) { }
 
   topList: any;
+  routerPath:string="";
   ngOnInit() {
-    console.log(this.route.routeConfig)
+    console.log("planform:", this.planform.pathname);
+    this.route.events.subscribe(data => {
+      if (data instanceof NavigationEnd) {
+        console.log(data.url);
+        this.routerPath=data.url;
+      }
+    })
     this.httpservice.getTopList().subscribe(data => {
       this.topList = data;
       console.log(data);
     })
   }
-  ngOnChanges() {
-    console.log(this.route.routeConfig)
+  // ngOnChanges() {
+  //   console.log(this.route.routeConfig)
+  // }
+  ngDoCheck() {
+    // console.log(this.route.firstChild.url._value[0].path)
+    // this.route.fragment.subscribe(data => {
+    //   console.log(data);
+    // })
+    // this.route.url.subscribe(url => console.log(url[0].path));
   }
 
 }
