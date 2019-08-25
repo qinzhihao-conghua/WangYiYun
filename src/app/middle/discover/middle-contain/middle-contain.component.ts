ddc8271feba22ee4f97544204c564b0030d6cbe9
qinzhihao-conghua <1043419697@qq.com>
2019-08-25 16:10:40 +0800

    布局，数据

--- src/app/middle/discover/middle-contain/middle-contain.component.ts
+++ src/app/middle/discover/middle-contain/middle-contain.component.ts
@@ -11,5 +11,81 @@ export class MiddleContainComponent implements OnInit {
 
   ngOnInit() {
   }
+  XDSJDATA: any[] = [
+    [
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子1"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "傻狍子2"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子3"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "傻狍子4"
+      }, {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子5"
+      }
+    ],
+    [
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子6"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "傻狍子7"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子8"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "傻狍子9"
+      }, {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子10"
+      }
+    ],
+    [
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子11"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "傻狍子12"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子13"
+      },
+      {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "傻狍子14"
+      }, {
+        imgSrc: "../../../../assets/images/狗子.jpg",
+        desc: "二狗子15"
+      }
+    ],
+  ]
 
+  scroll(index: string) {
+    if (index == "0") {
+      return "one";
+    }
+    else if(index == "1"){
+      return "two";
+    }
+    else if(index == "2"){
+      return "three";
+    }
+  }
 }
